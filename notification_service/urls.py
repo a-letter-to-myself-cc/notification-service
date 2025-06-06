1df1e670d50b1c723ae7d7313150c8a64fdbd141
Yeongeunn <nye0817@gmail.com>
2025-06-06 20:33:29 +0900

    feat: 헬스체크 urls 수정

--- notification_service/urls.py
+++ notification_service/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from notify.views import health_check  # ✅ 이거 맞아
 
 urlpatterns = [
-    path("notify/", include("notify.urls")),  # ✅ 앱 URL 포함
+    path('notify/', include('notify.urls')),  # 앱 내부 URL 등록
+    path('health/', health_check),            # ✅ 루트 health 경로 등록!
 ]
