3176925f39812b2ce125f26dc901466202efb5ef
Yeongeunn <nye0817@gmail.com>
2025-06-06 17:09:54 +0900

    feat: views.py urls.py health check 추가

--- notify/views.py
+++ notify/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+from rest_framework.decorators import api_view
 from notify.tasks import send_notification
 import json
 
@@ -21,3 +22,7 @@ def email_notification_api(request):
 
 def test_notification_api(request):
     return JsonResponse({'message': 'Notification sent!'})
+
+@api_view(['GET'])
+def health_check(request):
+    return JsonResponse({"status": "ok"})
